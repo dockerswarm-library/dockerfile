df02063bf277bd5086501302e0e1b5832156aacf
Socheat Sok <socheatsok78@gmail.com>
2024-05-23 10:59:42 +0700

    Update dockerswarm_get_network_ip.sh

--- hacks/dockerswarm_get_network_ip.sh
+++ hacks/dockerswarm_get_network_ip.sh
@@ -3,18 +3,29 @@
 # Do a reverse lookup on the IP address to get the network name
 function dockerswarm_get_network_ip() {
     local network_name=$1
+    if [ -z "$network_name" ]; then
+        echo "[dockerswarm_get_network_ip]: command line is not complete, network name is required"
+        return 1
+    fi
     # Loop through assigned IP addresses to the host
     for ip in $(hostname -i); do
         # Query the PTR record for the IP address
         local ptr_record=$(host "$ip" | cut -d ' ' -f 5)
+        # If the PTR record is empty, skip to the next IP address
+        if [ -z "$ptr_record" ]; then
+            continue
+        fi
         # Filter the PTR record to get the network name
         local service_network=$(echo "$ptr_record" | cut -d '.' -f 4)
         # Check if the network name matches the input network name
         if [[ "$service_network" == *"$network_name" ]]; then
             echo "$ip"
-            break
+            return
         fi
     done
+
+    echo "[dockerswarm_get_network_ip]: can't find network '$network_name'"
+    return 2
 }
 
 dockerswarm_get_network_ip "$@"
